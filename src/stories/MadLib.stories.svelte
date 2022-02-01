da18d2571ba73c14e1eba780a84a4ba871d1a3da
MKenz0 <miguel7vj@gmail.com>
2022-02-01 12:58:51 +0100

    Creation of the Mad Lib component

---
+++ src/stories/MadLib.stories.svelte
@@ -0,0 +1,35 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import MadLib from "./MadLib.svelte";
+</script>
+
+
+  <Meta
+    title="Mad Lib"
+    component={MadLib}
+    argTypes={{
+      backgroundColor: { control: "color" },
+      size: {
+        control: { type: 'select' },
+        options: ['small', 'medium', 'large'],
+      },
+    }}
+  />
+
+
+  <Template let:args>
+    <svg width="300" height="300" style="border:1px gray solid">
+        <MadLib
+            variable = {args.variable}
+        />
+    </svg>
+  </Template>
+
+
+  <Story
+    name="Example 1"
+    args={{
+        variable: 10
+    }}
+  />
+

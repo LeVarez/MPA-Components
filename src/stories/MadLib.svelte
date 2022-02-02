00477b9c1b9e3ff74a25ceb16693f185441e5ff8
MKenz0 <miguel7vj@gmail.com>
2022-02-02 10:20:40 +0100

    Listbox started

--- src/stories/MadLib.svelte
+++ src/stories/MadLib.svelte
@@ -1,11 +1,52 @@
 <script lang="ts">
+    import DropdownArrow from "./DropdownArrow.svelte";
+    import MadLibSelector from "./MadLibSelector.svelte";
     export let variable: number;
+
+    let listboxVisible = false;
+    let buttonText = "MPA manager"
+
+const click = () => {
+    buttonText = "help finding examples"
+}
+
+
 </script>
 
-<div class="article">
-    <h2>Helloa dfsfsdfsdfsdfsdfsfsdsdf</h2>
+<div class="article" on:click={click}>
+    <p>I am an <MadLibSelector/> and want help finding examples to evaluate decisions my governments needs to make</p>
 </div>
 
 <style>
 
+.button {
+    display: inline-block;
+    vertical-align: middle;
+    border-radius: 20px;
+    background-color: #F9F9F9;
+    font-size: 20;
+    color: black;
+    border: none;
+    padding: 0.35rem 0.7rem 0.35rem 0.7rem;
+    cursor: pointer;
+    font: inherit;
+    filter: drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.15));
+}
+
+.arrow {
+    display: inline-block;
+    vertical-align: top;
+}
+
+.article {
+    background-color: #F9F9F9;
+    padding-top: 100px;
+    padding-bottom: 100px;
+    padding-left: 50px;
+    font-family: 'Montserrat';
+    font-size: 20;
+    font-weight: normal;
+}
+
 </style> 
+
---
+++ src/stories/MadLibSelector.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+    import DropdownArrow from "./DropdownArrow.svelte";
+
+    let buttonText = "MPA manager";
+    let listboxVisible = false;
+
+    const click = () => {
+        listboxVisible = !listboxVisible;
+    }
+
+</script>
+
+<div class="selector-area" on:click={click}>
+    <div class="arrow">
+        <DropdownArrow/>
+    </div>
+    {buttonText}
+</div>
+
+<div class={listboxVisible ? "listbox-visible" : "listbox-hidden"}>
+    <div class="selector-option" on:click={click}>
+        {buttonText}
+    </div>
+    <div class="selector-option" on:click={click}>
+        {buttonText}
+    </div>
+    <div class="selector-option" on:click={click}>
+        {buttonText}
+    </div>
+</div>   
+
+
+<style>
+
+    .selector-option {
+        display: block;
+        border-radius: 20px;
+        background-color: #F9F9F9;
+        color: black;
+        border: none;
+        padding: 0.35rem 0.7rem 0.35rem 0.7rem;
+        cursor: pointer;
+        filter: drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.15));
+        font-family: 'Montserrat';
+        font-size: 20;
+        font-weight: normal; 
+    }
+
+    .selector-area {
+        display: inline-block;
+        vertical-align: middle;
+        border-radius: 20px;
+        background-color: #F9F9F9;
+        color: black;
+        border: none;
+        padding: 0.35rem 0.7rem 0.35rem 0.7rem;
+        cursor: pointer;
+        filter: drop-shadow(0px 1px 6px rgba(0, 0, 0, 0.15));
+        font-family: 'Montserrat';
+        font-size: 20;
+        font-weight: normal;
+    }
+
+    .listbox-visible {
+        position: absolute;
+        display: block;
+
+    }
+
+    .listbox-hidden {
+        display: none;
+    }
+
+    .arrow {
+        display: inline-block;
+        vertical-align: top;
+    }
+
+</style>

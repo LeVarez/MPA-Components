3a68423c29d064f8dadd74a47e28754758fe96f1
MKenz0 <miguel7vj@gmail.com>
2022-02-09 14:34:51 +0100

    dasdasasd

---
+++ src/stories/MainPreviewCarousel.stories.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import MainPreviewCarousel from "./MainPreviewCarousel.svelte";
+                    
+</script>
+
+<Meta
+    title="Carousel (Landing page)"
+    component={MainPreviewCarousel}
+    argTypes={{
+        backgroundColor: { control: "color" },
+        size: {
+        control: { type: 'select' },
+        options: ['small', 'medium', 'large'],
+        },
+    }}
+/>
+
+<Template let:args>
+    <MainPreviewCarousel/>
+</Template>
+
+
+<Story
+    name="Example 1"
+    args={{ }}
+/>
---
+++ src/stories/MainPreviewCarousel.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+    
+    import Carousel from 'svelte-carousel';
+    import MainPreviewCard from "./MainPreviewCard.svelte";
+    let tagArray = [{tag: "Blue economy", alt: ""}, 
+                    {tag: "MPAs", alt: ""}, 
+                    {tag: "Blue growth", alt: ""}, 
+                    {tag: "Ocean conservation", alt: ""}, 
+                    {tag: "Sustainable development", alt: ""}, 
+                    {tag: "Nature-based solutions", alt: ""}, 
+                    {tag: "All biomes", alt: ""}];
+
+</script>
+
+<div class="carousel-container">
+
+    <Carousel
+        particlesToShow={3}
+        particlesToScroll={1}
+        dots={false}
+    >
+
+        <div class="card">
+            <MainPreviewCard
+                cardType= 'chapter'
+                previewImage= "/img/chapter-preview-image.png"
+                category= 'Sustainable financing'
+                title= 'What should MPA managers know about the blue economy and business planning?'
+                tags={tagArray} 
+            />
+        </div>
+
+
+        <div class="card">
+            <MainPreviewCard
+                cardType= 'chapter'
+                previewImage= "/img/chapter-preview-image.png"
+                category= 'Sustainable financing'
+                title= 'What should MPA managers know about the blue economy and business planning?'
+                tags={tagArray} 
+            />
+        </div>
+
+        <div class="card">
+            <MainPreviewCard
+                cardType= 'chapter'
+                previewImage= "/img/chapter-preview-image.png"
+                category= 'Sustainable financing'
+                title= 'What should MPA managers know about the blue economy and business planning?'
+                tags={tagArray} 
+            />
+        </div>
+
+    </Carousel>
+</div>
+
+<style>
+
+    .carousel-container {
+        max-width: 2000px;
+    }
+
+    .card {
+        margin-left: 100px;
+    }
+
+</style>

4ab1ac4d3db3500a62bfd9719ea363be987bd694
MKenz0 <miguel7vj@gmail.com>
2022-02-09 08:56:10 +0100

    Tiny card creation

---
+++ src/stories/TinyPreviewCard.stories.svelte
@@ -0,0 +1,39 @@
+<script>
+    import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+    import TinyPreviewCard from "./TinyPreviewCard.svelte";
+
+    let tagArray = [{tag: "Blue economy"}, 
+                    {tag: "MPAs"}, 
+                    {tag: "Blue growth"}, 
+                    {tag: "Ocean conservation"}];
+                    
+</script>
+
+<Meta
+    title="Tiny Preview Card"
+    component={TinyPreviewCard}
+    argTypes={{
+        backgroundColor: { control: "color" },
+        size: {
+        control: { type: 'select' },
+        options: ['small', 'medium', 'large'],
+        },
+    }}
+/>
+
+<Template let:args>
+    <TinyPreviewCard
+        previewImage = {args.previewImage}
+        title = {args.title}
+        tags = {args.tags}
+    />
+</Template>
+
+
+<Story
+    name="Example 1"
+    args={{
+            previewImage: "/img/case-study-preview-image.png",
+            title: 'How can I improve communications to support sustainable financing?',
+            tags: tagArray }}
+/>
---
+++ src/stories/TinyPreviewCard.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import TagContainer from "./TagContainer.svelte";
+
+    export let tags: {tag: string, alt: string}[];
+    export let title: string;
+    export let previewImage: string;
+
+</script>
+
+<div class="lol" tabindex="0">
+
+</div>
+
+<style>
+
+    
+</style>
